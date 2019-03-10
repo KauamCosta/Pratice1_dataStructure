25640c0cf3f172234dc202bb0d2b5085b81de192
Kauam da Silva Costa <47905012+KauamCosta@users.noreply.github.com>
2019-03-09 22:19:31 -0300

    Atividade 1 e 2
    Comentadas e corrigidas

--- Pratica 1/Atividade1/Exercicio1.c
+++ Pratica 1/Atividade1/Exercicio1.c
@@ -1,57 +1,57 @@
-#include <stdio.h>	//Declaração de bibliotecas
+#include <stdio.h>													// Declaração de bibliotecas
 #include <stdlib.h>
 #include <locale.h>
 
 float calc(float salario, float *Imposto);
 
-int main(){ //Metodo Principal
+int main(){															// Metodo Principal
 	setlocale(LC_ALL, "");
 	float salario = 0,
 		NovoSalario = 0,
-		Imposto = 0; //Declaração das variaveis
+		Imposto = 0;												// Declaração das variaveis
 
-	printf_s("Digite o salário: "); //Solicitando salario
-	scanf_s("%f", &salario);		//Recolhendo valor digitado
-	NovoSalario = calc(salario, &Imposto);	//Chama o metodo calc aplicando o valor digitado, aplicando seu retorno em NovoSalario
-	printf_s("Seu salário antigo é de: R$%.2f \n", salario);	//Monstra seu antigo salario
-	printf_s("Seu salário atual é de: R$%.2f \n", NovoSalario);	//Mostra seu salario atual
-	printf_s("O Imposto devido é de: R$%.2f \n", Imposto);		// Mostra imposto devido
+	printf_s("Digite o salário: ");									// Solicitando salario
+	scanf_s("%f", &salario);										// Recolhendo valor digitado
+	NovoSalario = calc(salario, &Imposto);							// Chama o metodo calc aplicando o valor digitado, aplicando seu retorno em NovoSalario
+	printf_s("Seu salário antigo é de: R$%.2f \n", salario);		// Monstra seu antigo salario
+	printf_s("Seu salário atual é de: R$%.2f \n", NovoSalario);		// Mostra seu salario atual
+	printf_s("O Imposto devido é de: R$%.2f \n", Imposto);			// Mostra imposto devido
 
-	return 0;	//Retorno no metodo principal
+	return 0;														// Retorno no metodo principal
 }
 
-float calc(float salario, float *Imposto) { //Metodo para o calculo
+float calc(float salario, float *Imposto) {							// Metodo para o calculo
 	float 
 			taxa = 869.36,
 			NovoSalario = salario,
-			Aux = 0;			//Declaração das variaveis
+			Aux = 0;												// Declaração das variaveis
 
-	if (NovoSalario <= 1903.98) {}				//Verificação do salario
-	  else if (NovoSalario > 1903.98 && NovoSalario <= 2826.65) {
+	if (NovoSalario <= 1903.98) {}									// Verificação do salario
+	  else if (NovoSalario > 1903.98 && NovoSalario <= 2826.65) {	// Verifica se esta entre 1903.98 e 2826.65
 
-		Aux = (NovoSalario * 7.5) / 100;
-		*Imposto = Aux - 142.80;
-		NovoSalario = NovoSalario - Aux;
+		Aux = (NovoSalario * 7.5) / 100;							// Salva o valor na variavel auxiliar
+		*Imposto = Aux - 142.80;									// Salvando valor do imposto
+		NovoSalario = NovoSalario - Aux;							// Guarda do novo salario
 
-	} else if (NovoSalario > 2826.65 && NovoSalario <= 3751.05) {
+	} else if (NovoSalario > 2826.65 && NovoSalario <= 3751.05) {	// Verifica se esta entre 2826.65 e 3751.05
 		
-		Aux = (NovoSalario * 15) / 100;
-		*Imposto = Aux - 354.80;
-		NovoSalario = NovoSalario - Aux;
+		Aux = (NovoSalario * 15) / 100;								// Salva o valor na variavel auxiliar
+		*Imposto = Aux - 354.80;									// Salvando valor do imposto
+		NovoSalario = NovoSalario - Aux;							// Guarda do novo salario
 
-	} else if (NovoSalario > 3751.05 && NovoSalario <= 4664.68) {
+	} else if (NovoSalario > 3751.05 && NovoSalario <= 4664.68) {	// Verifica se esta entre 3751.05 e 4664.68
 
-		Aux = (NovoSalario * 22.5) / 100;
-		*Imposto = Aux - 636.13;
-		NovoSalario = NovoSalario - Aux;
+		Aux = (NovoSalario * 22.5) / 100;							// Salva o valor na variavel auxiliar
+		*Imposto = Aux - 636.13;									// Salvando valor do imposto
+		NovoSalario = NovoSalario - Aux;							// Guarda do novo salario
 
 	} else {
 
-		Aux = (NovoSalario * 27.5) / 100;
-		*Imposto = Aux - 869.36;
-		NovoSalario = NovoSalario - Aux;
+		Aux = (NovoSalario * 27.5) / 100;							// Salva o valor na variavel auxiliar
+		*Imposto = Aux - 869.36;									// Salvando valor do imposto
+		NovoSalario = NovoSalario - Aux;							// Guarda do novo salario
 
 	}
 
-	return (NovoSalario);	//Retorno do metodo
+	return (NovoSalario);											// Retorno e finalização do metodo
 }
--- Pratica 1/Atividade2/Exercicio2.c
+++ Pratica 1/Atividade2/Exercicio2.c
@@ -1,89 +1,99 @@
-#include <stdio.h>	//Declaração de bibliotecas
+#include <stdio.h>											// Declaração de bibliotecas
 #include <stdlib.h>
 #include <locale.h>
 #include <windows.h>
 
-int inPar(int numero[], int aux, int *RU);
 
-int main(){
+int main(){													// Declaração do metodo principal
 
-	setlocale(LC_ALL, "");
+	setlocale(LC_ALL, "");									// Funçao que aceita acentos no prompt
 	int 
 		i = 0,
 		N[100],
 		Par[100],
 		Impar[100],
 		Aux = 0,
-		RU = 0;
+		RU = 0;												// Declaração das variaveis
 
-	printf_s("Digite os dois ultimos digitos do seu RU: ");
-	scanf_s("%d", &RU);
+	printf_s("Digite os dois ultimos digitos do seu RU: "); // Solicitação do final do RU
+	scanf_s("%d", &RU);										// Recolhendo valor digitado
 
-	if (RU < 10 || RU > 99) {
+	if (RU < 10 || RU > 99) {								// Verificação se pode ser o final do RU
 
-		printf_s("\nValor indisponivel\n");
-		Sleep(3000);
-		system("cls");
-		main();
-		return 0;
+		printf_s("\nValor indisponivel\n");					// Informa que não é um RU disponivel
+		Sleep(3000);										// Trava o prompt por 3 seg
+		system("cls");										// Limpa o prompt
+		main();												// Executa o metodo main
+		return 0;											// Finaliza o metodo
 
 	}
 	else {
 
-		printf_s("Digite o tamanho do vetor: ");
-		scanf_s("%d", &Aux);
-		if (Aux < 100 && Aux > 0) {
+		printf_s("Digite o tamanho do vetor: ");			// Solicita o tamanho do vetor
+		scanf_s("%d", &Aux);								// Recolhendo valor digitado
+		if (Aux < 100 && Aux > 0) {							// Limita o vetor e não permite numeros negativos
 
-			for (i = 0; i < Aux; i++) {
-				printf_s("Digite o %dº valor: ", i+1);
-				scanf_s("%d", &N[i]);				
+			for (i = 0; i < Aux; i++) {						// Loop para preenchimento do vetor
+				printf_s("Digite o %dº valor: ", i+1);		// Solicita os valores para preenchimento
+				scanf_s("%d", &N[i]);						// Recolher valor digitado
 			}
 
 		} else {
 
-			printf_s("\nValor indisponivel\n");
-			Sleep(3000);
-			system("cls");
-			main();
-			return 0;
+			printf_s("\nValor indisponivel\n");				// Informa que não é um RU disponivel
+			Sleep(3000);									// Trava o prompt por 3 seg
+			system("cls");									// Limpa o prompt
+			main();											// Executa o metodo main
+			return 0;										// Finaliza o metodo
 
 		}
-		inPar(N, Aux, &RU);
+		imPar(N, Aux, &RU);									// Executa o metodo imPar enviando 3 parametros
 	}
-	return 0;
+	return 0;												// Finaliza metodo
 }
 
-int inPar(int numero[], int aux, int *RU) {
+int imPar(int numero[], int aux, int *RU) {					// Declara o metodo imPar
 
 	int i = 0,
 		ip = 0,
 		p = 0,
 		Impar[100],
-		Par[100];
+		Par[100];											// Declara as variaveis do metodo inPar
 
-	for (i = 0; i < aux; i++) {
+	for (i = 0; i < aux; i++) {								// Loop para verificar se é impar ou par
 
-		if ((numero[i] % 2) != 0) {
+		if ((numero[i] % 2) != 0) {							// Verifica se mod do numero em questao é 0 (Par) ou 1 (Impar)
 
-			Impar[ip] = numero[i] * *RU;
-			ip++;
+			Impar[ip] = numero[i] * *RU;					// Guarda o numero impar já multiplicado pelo final do RU
+			ip++;											// Incrementar mais um no contador
 		} else {
-			Par[p] = numero[i];
-			p++;
+			Par[p] = numero[i];								// Guarda o numero par
+			p++;											// Incrementar mais um no contador
 		}
 
 	}
 
-	printf_s("Numeros impares:\n");
-	for (i = 0; i < ip; i++) {		
-		if (i == 0) {
-			printf_s(" __ \n|.0%d|", Impar[i]);
-		} else if (i == (ip-1)) {
-			printf_s("\n|%d|\n --", Impar[i]);
+	printf_s("Numeros impares:\n");							// Informa que os numeros que serao imprimidos serao os impares
+	for (i = 0; i < ip; i++) {								// Loop para impressão dos numeros impares
+		if (i == 0) {										// Verifica se é o primeiro numero
+			printf_s(" __ \n|%d|", Impar[i]);				// Caso seja o primeiro ele imprime dois caracteres para fechar em cima a caixa
+		} else if (i == (ip-1)) {							// Verifica se é o ultimo numero
+			printf_s("\n|%d|\n --\n", Impar[i]);			// Caso seja o ultimo nume ele imprime dois caracteres para fechar em baixo
 		} else {
-			printf_s("\n|%d|", Impar[i]);
+			printf_s("\n|%d|", Impar[i]);					// Imprime o numero com barras laterais
 		}
 	}
 
-	return 0;
+	printf_s("Numeros pares:\n");							// Informa que os numeros que serao imprimidos serao os pares
+	for (i = 0; i < p; i++) {								// Loop para impressão dos numeros pares
+		if (i == 0) {										// Verifica se é o primeiro numero
+			printf_s(" __ \n|%d|", Par[i]);					// Caso seja o primeiro ele imprime dois caracteres para fechar em cima a caixa
+		} else if (i == (p - 1)) {							// Verifica se é o ultimo numero
+			printf_s("\n|%d|\n --\n", Par[i]);				// Caso seja o ultimo nume ele imprime dois caracteres para fechar em baixo
+		} else {
+			printf_s("\n|%d|", Par[i]);						// Imprime o numero com barras laterais
+		}
+	}
+
+	return 0;												// Finaliza o metodo
 }
